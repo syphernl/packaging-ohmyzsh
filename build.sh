30d40e20ef28f382c763e7e8440d33302574e698
Frank Klaassen <frank@cloudright.nl>
2015-05-22 15:39:32 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+source vars.sh
+
+echo "Cleanup old.."
+rm *.deb
+
+sssssssssssssssssssssssssss
+
+git pull origin master | grep "Already up-to-date"
+
+# If git didn't tell us we are up-to-date, we need to compile a fresh package
+if [ $? -ne 0 ]; then
+  echo "Building package..."
+  ZSH_REVISION=$(git rev-parse --short HEAD)
+
+  cd -
+  fpm -s dir -t deb \
+  --url "http://ohmyz.sh/" \
+  --description "A community-driven framework for managing your zsh configuration." \
+  --license "MIT" \
+  --maintainer "$PKG_MAINTAINER" \
+  --vendor "$PKG_VENDOR" \
+  --iteration "$ZSH_REVISION" \
+  -d zsh \
+  -a all \
+  -n oh-my-zsh usr
+fi
---
+++ prepare.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir usr/local/share/
+cd usr/local/share/
+git clone https://github.com/robbyrussell/oh-my-zsh.git
