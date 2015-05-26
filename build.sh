7a436042f3b6629f8a3811a993c4a5487c719c7a
Frank Klaassen <frank@cloudright.nl>
2015-05-26 08:52:35 +0200

    Check update script

--- build.sh
+++ build.sh
@@ -4,23 +4,17 @@ source vars.sh
 echo "Cleanup old.."
 rm *.deb
 
-cd usr/local/share/
-git pull origin master | grep "Already up-to-date"
+echo "Building package..."
+ZSH_REVISION=$(git rev-parse --short HEAD)
 
-# If git didn't tell us we are up-to-date, we need to compile a fresh package
-if [ $? -ne 0 ]; then
-  echo "Building package..."
-  ZSH_REVISION=$(git rev-parse --short HEAD)
-
-  cd -
-  fpm -s dir -t deb \
-  --url "http://ohmyz.sh/" \
-  --description "A community-driven framework for managing your zsh configuration." \
-  --license "MIT" \
-  --maintainer "$PKG_MAINTAINER" \
-  --vendor "$PKG_VENDOR" \
-  --iteration "$ZSH_REVISION" \
-  -d zsh \
-  -a all \
-  -n oh-my-zsh usr
-fi
+cd -
+fpm -s dir -t deb \
+--url "http://ohmyz.sh/" \
+--description "A community-driven framework for managing your zsh configuration." \
+--license "MIT" \
+--maintainer "$PKG_MAINTAINER" \
+--vendor "$PKG_VENDOR" \
+--iteration "$ZSH_REVISION" \
+-d zsh \
+-a all \
+-n oh-my-zsh usr
